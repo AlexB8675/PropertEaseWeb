a4ee1fb85939b7f9108620438ac2378bbda470bf
AlexB8675 <alexandr.benbaccar001@studenti.uniparthenope.it>
2023-11-10 00:59:27 +0100

    small abstraction

--- backend/main.js
+++ backend/main.js
@@ -3,6 +3,18 @@ const cors = require('cors');
 const sqlite = require('sqlite3').verbose();
 const app = express();
 
+function registerApiEndpoint(app, database, info) {
+    app.get(info.endpoint, (request, result) => {
+        database.all(info.query, info.parameters(request), (error, rows) => {
+            if (error) {
+                console.error(error);
+            }
+            console.log(request.params);
+            info.callback(result, rows);
+        });
+    });
+}
+
 const database = new sqlite.Database('../main.sqlite', (error) => {
     if (error) {
         console.error(error.message);
@@ -10,12 +22,28 @@ const database = new sqlite.Database('../main.sqlite', (error) => {
     console.log('connected to the main database.');
 });
 app.use(cors());
-app.get('/api/data/houses', (request, result) => {
-    database.all('select * from House', (error, rows) => {
-        if (error) {
-            console.error(error);
-        }
+registerApiEndpoint(app, database, {
+    endpoint: '/api/data/houses',
+    query: 'select * from House',
+    parameters: _ => [],
+    callback: (result, rows) => {
         result.send(rows);
-    });
+    }
+});
+registerApiEndpoint(app, database, {
+    endpoint: '/api/data/houses/id/:houseId',
+    query: 'select * from House where id = ?',
+    parameters: (request) => [request.params.houseId],
+    callback: (result, rows) => {
+        result.send(rows);
+    }
+});
+registerApiEndpoint(app, database, {
+    endpoint: '/api/data/houses/city/:city',
+    query: 'select * from House where city = ?',
+    parameters: (request) => [request.params.city],
+    callback: (result, rows) => {
+        result.send(rows);
+    }
 });
 app.listen(8080);
