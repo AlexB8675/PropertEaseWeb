532673ee977af11138a184c52b090d4b33999d25
AlexB8675 <70755608+AlexB8675@users.noreply.github.com>
2023-11-13 16:33:07 +0100

    adjusted grid layout, fixed most of the problems with perspective and scale. triangle detach problem remains

--- js/main.js
+++ js/main.js
@@ -66,9 +66,10 @@ $(() => {
     }).done((data) => {
         const mainCardContainer = $('.card-container > .wrapper');
         const cardTemplate = (address, city, cap, contract, price, image, type) => `
-            <div class="card tilt">
+            <div class="card">
                 <div>
                     <img draggable="false" src="${image}" alt>
+                    <div class="generic-triangle"></div>
                 </div>
                 <div class="content">
                     <div class="text">${type + " for " + ((contract) ? "Sale" : "Rent")}</div>
@@ -91,12 +92,26 @@ $(() => {
                 )
             );
             const cardImage = card.find('img');
+            let mouseOverTimeout = -1;
             card
                 .on('mouseover', () => {
-                    cardImage.css('transform', 'scale(1)');
+                    if (mouseOverTimeout !== -1) {
+                        clearTimeout(mouseOverTimeout);
+                    }
+                    const parent = cardImage.parent();
+                    cardImage
+                        .css({ 'transform': 'translateZ(0) scale(1)' })
+                        .addClass('shimmer-effect');
+                    parent.css({ 'background': 'aliceblue' });
+                    mouseOverTimeout = setTimeout(() => {
+                        parent.css({ 'background': '' });
+                        mouseOverTimeout = -1;
+                    }, 500);
                 })
                 .on('mouseleave', () => {
-                    cardImage.css('transform', 'scale(1.05)');
+                    cardImage
+                        .css({ 'transform': 'translateZ(1px) scale(1.05)' })
+                        .removeClass('shimmer-effect');
                 })
                 .on('mousemove', (event) => {
                     const height = card.height();
@@ -107,17 +122,18 @@ $(() => {
                     const yRotation = 2 * ((xVal - width / 2) / width);
                     const xRotation = -2 * ((yVal - height / 2) / height);
 
-                    const transformString = 'perspective(600px) scale(1.02) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)';
+                    const rotationString = `rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
+                    const transformString = `perspective(600px) translateZ(0) scale(1.02) ${rotationString}`;
                     card.css('transform', transformString);
                 })
                 .on('mouseout', () => {
-                    card.css('transform', 'perspective(600px) scale(1) rotateX(0) rotateY(0)');
+                    card.css('transform', 'perspective(600px) translateZ(1px) scale(1) rotateX(0) rotateY(0)');
                 })
                 .on('mousedown', () => {
-                    card.css('transform', 'perspective(600px) scale(0.98) rotateX(0) rotateY(0)');
+                    card.css('transform', 'perspective(600px) translateZ(1px) scale(0.98) rotateX(0) rotateY(0)');
                 })
                 .on('mouseup', () => {
-                    card.css('transform', 'perspective(600px) scale(1.02) rotateX(0) rotateY(0)');
+                    card.css('transform', 'perspective(600px) translateZ(1px) scale(1.02) rotateX(0) rotateY(0)');
                 });
             mainCardContainer.append(card);
         }
